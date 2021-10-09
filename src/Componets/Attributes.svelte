35b2e166ae39ea3214c985d7c815837c9b353c5b
namin-amin <67176003+namin-amin@users.noreply.github.com>
2021-10-09 14:10:51 +0530

    updated attributes

--- src/Componets/Attributes.svelte
+++ src/Componets/Attributes.svelte
@@ -6,3 +6,5 @@
 <a href="https://icons8.com/icon/45586/download-resume"
   >Download Resume icon by Icons8</a
 >
+<a href="https://icons8.com/icon/59820/laptop">Laptop icon by Icons8</a>
+<a href="https://icons8.com/icon/53388/mail">Mail icon by Icons8</a>
