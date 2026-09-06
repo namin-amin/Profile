b6e7ea2405ae0dae507a7830304f0d0d0dc53678
namin-amin <navaneesh185@gmail.com>
2026-09-06 12:51:32 +0530

    feat: add support for custom domain in GitHub Pages deployment; update README with configuration details

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.GITHUB_ACTIONS
+  base: process.env.VITE_BASE_PATH ?? (process.env.GITHUB_ACTIONS
     ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Profile'}/`
-    : '/'
+    : '/')
 });
